9afc26794677fae9b7f04b4686faf6d86fa757ac
nluzinda <natasha.luzinda@gmail.com>
2020-12-28 18:32:13 +0000

    Add ships_left image

--- alien_invasion.py
+++ alien_invasion.py
@@ -73,6 +73,8 @@ class AlienInvasion:
             self.stats.reset_stats()
             self.stats.game_active = True
             self.sb.prep_score()
+            self.sb.prep_level()
+            self.sb.prep_ships()
 
             # Get rid of any remaining aliens and bullets
             self.aliens.empty()
@@ -138,6 +140,10 @@ class AlienInvasion:
             self._create_fleet()
             self.settings.increase_speed()
 
+            # Increase level
+            self.stats.level += 1
+            self.sb.prep_level()
+
     def _update_aliens(self):
         """Update the positions of all aliens in the fleet"""
         """Check if the fleet is at an edge, then update the positions of all aliens in the fleet"""
@@ -154,8 +160,9 @@ class AlienInvasion:
     def _ship_hit(self):
         """Respond to the ship being hit by an alien"""
         if self.stats.ships_left > 0:
-            # Decrement ships_left
+            # Decrement ships_left, and pdate scoreboard. 
             self.stats.ships_left -= 1
+            self.sb.prep_ships()
 
             # Get rid of any remaining aliens and bullets
             self.aliens.empty()
